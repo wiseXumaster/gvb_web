ac0b2eef22bdbce73678fcf0a4470afbf911268e
wise <3058524082@qq.com>
2024-10-29 17:17:17 +0800

    下拉菜单点击事件

--- src/views/admin/admin.vue
+++ src/views/admin/admin.vue
@@ -29,17 +29,17 @@
                                 <i class="fa fa-angle-down"></i>
                             </a>
                             <template #overlay>
-                                <a-menu>
-                                    <a-menu-item>
+                                <a-menu @click="menuClick">
+                                    <a-menu-item key="user_center">
                                         <a href="javascript:;">个人中心</a>
                                     </a-menu-item>
-                                    <a-menu-item>
+                                    <a-menu-item key="my_messages">
                                         <a href="javascript:;">我的消息</a>
                                     </a-menu-item>
-                                    <a-menu-item>
+                                    <a-menu-item key="article_list">
                                         <a href="javascript:;">文章列表</a>
                                     </a-menu-item>
-                                    <a-menu-item>
+                                    <a-menu-item key="logout">
                                         <a href="javascript:;">注销退出</a>
                                     </a-menu-item>
                                 </a-menu>
@@ -55,6 +55,17 @@
 </template>
 
 <script setup>
+import {useRouter} from "vue-router"
+const router =useRouter()
+function menuClick({key}){
+    if(key==="logout"){
+        console.log("logout")
+        return
+    }
+    router.push({
+        name: key
+    })
+}
 </script>
 
 <style lang="scss">
